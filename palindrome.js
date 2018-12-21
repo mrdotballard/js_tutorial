4db3df1792c01670364aadc7b57c25e0a0c29e19
Matthew <mr.ballard@outlook.com>
2018-12-21 16:06:33 +0000

    Fix processedContent error

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+// Adds Reverse to all strings.
+String.prototype.reverse = function reverse() {
+  return Array.from(this).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function() {
+    return processedContent === this.processedContent().reverse();
+  }
+}
